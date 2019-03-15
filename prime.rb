7163e2491d565a144fd6d92462d7530f1a806e04
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-15 17:05:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,10 @@
 # Add  code here!
 def prime? num
-  
   pos_inf= 1.0/0
 
   if num <= pos_inf
     val= num
-    if val % num == 0 && val == num && val / 1 == num
+    if val % num == 0 && val / 1 == num
       true
     else
       false
