2196b973b9fdcf450dd3854964161d5f6d3ef288
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-15 16:36:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,13 @@
 # Add  code here!
 def prime? num
+  f_num= num.to_f
   neg_inf= -1.0/0
   pos_inf= 1.0/0
   range_val = neg_inf..pos_inf
   arr_num = range_val.to_a
-  if(arr_num.include?(num.to_f))
-    val= num.to_f
-    if(val % num == 0 && val == num)
+  if(arr_num.include?(f_num))
+    val= f_num
+    if(val % f_num == 0 && val == f_num)
       true
     else
       false
