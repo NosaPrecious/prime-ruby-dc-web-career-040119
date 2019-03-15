3ce0d129ccc4160dbc10a46644b01cba9be48e2e
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-15 16:16:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,11 @@
 def prime? num
   range_val = 2..100
   arr_num = range_val.to_a
-  if(arr_num.include?(num) && num)
+  if(arr_num.include?(num))
+    val= num
+    if(val % num == 0 && val != num)
+      true
+    else
+      false
+    end
 end
