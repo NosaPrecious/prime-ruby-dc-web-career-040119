1359f529c326fe2c6011941c8788afa2a1bd7529
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-15 16:59:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime? num
   neg_inf= -1.0/0
   pos_inf= 1.0/0
 
-  if num < pos_inf || num >= neg_inf
+  if num <= pos_inf || num >= neg_inf
     val= num
     if val % num == 0 && val == num
       true
