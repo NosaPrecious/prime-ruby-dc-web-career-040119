cff63e0cf086f431e93d2e7a3713cf60b9a6cc72
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-15 16:55:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,7 @@ def prime? num
   neg_inf= -1.0/0
   pos_inf= 1.0/0
   range_val = neg_inf..pos_inf
-  
-  arr_num = conv_range.to_a
+
   if(arr_num.include?(num))
     val= num
     if(val % num == 0 && val == num)
